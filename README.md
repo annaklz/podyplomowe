392cadbb0339eb94068f75da1537f40c6ce2c785
annaklz <annakluza@yahoo.com>
2021-11-27 14:39:20 +0100

    Dodatek obrazu w README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # podyplomowe
 
 ### Dowolny program na laboratorium Git 1
+
+![Dolomiti](https://live.staticflickr.com/2734/4210859570_dae97dda3f_b.jpg)
--- src/SomeClass.java
+++ src/SomeClass.java
@@ -1,5 +1,6 @@
 public class SomeClass {
     public static void main(String[] args) {
+        // Dowolny program na laboratorium Git 1
         System.out.println("Jestem dowolnym programem :)");
     }
 }
